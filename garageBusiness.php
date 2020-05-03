534e0a46ee634fa55bfc784850628f27cbfaca8b
iggi <ignatisnb@gmail.com>
2020-05-03 20:19:08 +0300

    park a truck

--- garageBusiness.php
+++ garageBusiness.php
@@ -1,16 +1,14 @@
 <?php
 require_once "vendor/autoload.php";
 
-$myGarage = new Garage(800, [WheeledVehicle::CATEGORY], [Car::TYPE, Truck::TYPE]);
 $timeUnit = Garage::TIMEUNIT;
+$myGarage = new Garage(800, [WheeledVehicle::CATEGORY], [Car::TYPE, Truck::TYPE]);
 $myGarage->setCostPer(2);
 
 $truck = new Truck("Toyota");
 $myGarage->park($truck);
 
-$nowEmptySpace = $myGarage->getEmptySpace();
-$parkedCount = $myGarage->getParkedCount();
-$myGarage->log("Parked: {$parkedCount} and Space left: {$nowEmptySpace}");
+$myGarage->log("Parked: {$myGarage->getParkedCount()} and Space left: {$myGarage->getEmptySpace()}");
 
 sleep(1); // lets pretend 1 second = 1 day
 $myGarage->log("One more {$timeUnit} has passed");
@@ -18,9 +16,7 @@ $myGarage->log("One more {$timeUnit} has passed");
 $car = new Car("Volkswagen");
 $myGarage->park($car);
 
-$nowEmptySpace = $myGarage->getEmptySpace();
-$parkedCount = $myGarage->getParkedCount();
-$myGarage->log("Parked: {$parkedCount} and Space left: {$nowEmptySpace}");
+$myGarage->log("Parked: {$myGarage->getParkedCount()} and Space left: {$myGarage->getEmptySpace()}");
 
 sleep(1);
 $myGarage->log("One more {$timeUnit} has passed");
@@ -33,6 +29,4 @@ try {
     echo $e->getMessage();
 }
 
-$nowEmptySpace = $myGarage->getEmptySpace();
-$parkedCount = $myGarage->getParkedCount();
-$myGarage->log("Parked: {$parkedCount} and Space left: {$nowEmptySpace}");
+$myGarage->log("Parked: {$myGarage->getParkedCount()} and Space left: {$myGarage->getEmptySpace()}");
